f7e99bf4dc9d307c565ff8867c7108366db0785f
Agustin Allamano Costa <allamanocostaagustin@gmail.com>
2023-01-25 21:18:12 -0300

    suma caso de test para los numeros X y XI

--- src/conversor.js
+++ src/conversor.js
@@ -1,10 +1,10 @@
 
 const conversorARomanos = (number) => {
-    const numerosRomanos = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"];
+    const numerosRomanos = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI"];
     for (let i=0; i <= numerosRomanos.length; i++) {
         if (i + 1 === number) {
-                  return numerosRomanos[i];
-              }
+                return numerosRomanos[i];
+            }
     }
     return undefined;
 }
