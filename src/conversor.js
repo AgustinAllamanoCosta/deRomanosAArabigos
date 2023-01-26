dd88c7118620408a15361f56a37df3b79f58a3dc
Federico Lencina <federiconahuellencina@gmail.com>
2023-01-25 21:07:03 -0300

    Added initial tests

---
+++ src/conversor.js
@@ -0,0 +1,12 @@
+
+const conversorARomanos = (number) => {
+    const numerosRomanos = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"];
+    for (let i=0; i <= numerosRomanos.length; i++) {
+        if (i + 1 === number) {
+                  return numerosRomanos[i];
+              }
+    }
+    return undefined;
+}
+
+module.exports = conversorARomanos;
